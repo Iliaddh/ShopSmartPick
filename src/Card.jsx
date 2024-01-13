b0fafcd33889a7e1138f3be542dcd6b52f8a52e8
Iliad Hemmatjoo <ilyadhemmatjoo2003@gmail.com>
2024-01-12 21:42:39 -0500

    link

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,7 +9,7 @@ function Card({img, price, content,title, link }) {
         <div className='justify-center flex'><img src={img} className='h-full max-w-40' alt="" /></div>
         <div className='p-2 flex justify-center'><p className='mt-12 text-md text-pretty max-w-lg'>{title}</p></div>
         <div className='flex justify-center p-4 mt-6 text-2xl'><p>${price}</p></div>
-        <div className=' mt-20  flex justify-center'><button className=' b-0 p-2 w-3/5 h-10 rounded-lg bg-gray-400 text-white  mb-0 '><a href={link} target="_blank">Visit Website</a></button></div>
+        <a href={link} target="_blank"><div className=' mt-20  flex justify-center' ><button className=' b-0 p-2 w-3/5 h-10 rounded-lg bg-gray-400 text-white  mb-0 '><a href={link} target="_blank">Visit Website</a></button></div></a>
         
       </div>
 
